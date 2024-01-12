6639e096f9c444e34ce2f0e17bb4f80f32579ee0
Asiphe04 <asiphendimlana40@gmail.com>
2024-01-12 15:46:41 +0200

    left with authentication

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -20,38 +20,41 @@ window.location.reload()
 }
 
 export default function EntryCard({id, name, year, type, varietal, rating, consumed, Date_consumed }: Props) {
-    return(
-      <table>
-      <thead>
-          <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Year</th>
-              <th>Type</th>
-              <th>Varietal</th>
-              <th>Rating</th>
-             {/* <th>Date Consumed</th>  */}
-              <th>Actions</th>
-          </tr>
+  return (
+    <table className="min-w-full border border-gray-300">
+      <thead className="bg-gray-200">
+        <tr>
+          <th className="py-2 px-4 border-b">ID</th>
+          <th className="py-2 px-4 border-b">Name</th>
+          <th className="py-2 px-4 border-b">Year</th>
+          <th className="py-2 px-4 border-b">Type</th>
+          <th className="py-2 px-4 border-b">Varietal</th>
+          <th className="py-2 px-4 border-b">Rating</th>
+          {/* <th className="py-2 px-4 border-b">Date Consumed</th> */}
+          <th className="py-2 px-4 border-b">Actions</th>
+        </tr>
       </thead>
       <tbody>
-          <tr>
-              <td>{id}</td>
-              <td>{name}</td>
-              <td>{year}</td>
-              <td>{type}</td>
-              <td>{varietal}</td>
-              <td>{rating}</td>
-               {/* <td>{Date_consumed}</td>  */}
-              <td>
-                  {consumed}
-                  <button onclick={() => deleteEntry(id)}>Delete</button>
-                  <a href={`/entry/edit?id=${id}`} role='button'>Edit</a>
-              </td>
-          </tr>
-        
+        <tr className="bg-white">
+          <td className="py-2 px-4 border-b">{id}</td>
+          <td className="py-2 px-4 border-b">{name}</td>
+          <td className="py-2 px-4 border-b">{year}</td>
+          <td className="py-2 px-4 border-b">{type}</td>
+          <td className="py-2 px-4 border-b">{varietal}</td>
+          <td className="py-2 px-4 border-b">{rating}</td>
+          {/* <td className="py-2 px-4 border-b">{Date_consumed}</td> */}
+          <td className="py-2 px-4 border-b">
+            {consumed}
+            <button className="ml-2 px-2 py-1 bg-red-500 text-white rounded" onClick={() => deleteEntry(id)}>
+              Delete
+            </button>
+            <a href={`/entry/edit?id=${id}`} role="button" className="ml-2 px-2 py-1 bg-blue-500 text-white rounded">
+              Edit
+            </a>
+          </td>
+        </tr>
       </tbody>
-  </table>
+    </table>
+  );
   
-    )
 }
