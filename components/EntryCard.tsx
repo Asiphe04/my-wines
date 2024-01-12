4373391c21fe1cd5b7215e74abdaf803c1bcffac
Asiphe04 <asiphendimlana40@gmail.com>
2024-01-12 13:29:27 +0200

    busy with styling

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -21,25 +21,37 @@ window.location.reload()
 
 export default function EntryCard({id, name, year, type, varietal, rating, consumed, Date_consumed }: Props) {
     return(
-      <article>
-        <header>
-        <h2>{id}</h2>
-            <h2>{name}</h2>
-        </header>
-        <p>{year}</p>
-        <p>{type}</p>
-        <p>{varietal}</p>
-        <p>{rating}</p> 
-        {/* <p>{Date_consumed}</p> */}
-       
-        <footer>
-            {consumed}
-            <button onClick={()=>deleteEntry(id)}>Delete</button>
-          
-            <a href={`/entry/edit?id=${id}`} role='button'>Edit </a>
-          
-          
-        </footer>
-      </article>
+      <table>
+      <thead>
+          <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Year</th>
+              <th>Type</th>
+              <th>Varietal</th>
+              <th>Rating</th>
+             {/* <th>Date Consumed</th>  */}
+              <th>Actions</th>
+          </tr>
+      </thead>
+      <tbody>
+          <tr>
+              <td>{id}</td>
+              <td>{name}</td>
+              <td>{year}</td>
+              <td>{type}</td>
+              <td>{varietal}</td>
+              <td>{rating}</td>
+               {/* <td>{Date_consumed}</td>  */}
+              <td>
+                  {consumed}
+                  <button onclick={() => deleteEntry(id)}>Delete</button>
+                  <a href={`/entry/edit?id=${id}`} role='button'>Edit</a>
+              </td>
+          </tr>
+        
+      </tbody>
+  </table>
+  
     )
 }
